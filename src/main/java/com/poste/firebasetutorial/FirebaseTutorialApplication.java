f88b3000ac767f510c12786460c6011d12a4125f
Shamuz <succesatchemi8@gmail.com>
2023-08-18 07:58:57 +0000

    premier commit| ajout de la dependance de firebase dans pom

---
+++ src/main/java/com/poste/firebasetutorial/FirebaseTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.poste.firebasetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poste/firebasetutorial/FirebaseTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poste.firebasetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
